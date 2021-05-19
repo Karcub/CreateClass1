d0dc2a611de386093e4b4453b2d2aaaea932e791
Karcub <rokaszemumenyecske@gmail.com>
2021-05-19 15:57:48 +0200

    Exercise 1: Create new Person class

---
+++ CreateClass1/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CreateClass1
+{
+    public class Person
+    {
+        public string Name;
+        public DateTime BirthDay;
+        public Gender Gender;
+
+        public Person(string name, DateTime birthDay, Gender gender)
+        {
+            Name = name;
+            BirthDay = birthDay;
+            Gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(BirthDay)}: {BirthDay}, {nameof(Gender)}: {Gender}";
+        }
+    }
+    
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
---
+++ CreateClass1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CreateClass1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person anna = new Person("Anna", new DateTime(1990, 01, 01), Gender.Female);
+            Console.WriteLine(anna);
+        }
+    }
+}
